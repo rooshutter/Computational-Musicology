f088fa72ee368d26b3f02b1f2df0c41f3ba08bc6
rooshutter <roos.hutter@student.uva.nl>
2023-02-22 18:45:15 +0100

    .

--- Hw2.R
+++ Hw2.R
@@ -2,35 +2,36 @@ library(tidyverse)
 library(spotifyr)
 library(flexdashboard)
 library(Cairo)
+library(compmus)
 
 Sys.setenv(SPOTIFY_CLIENT_ID = 'c6d2017616c34106a1b1c8407f7fbc6e')
 Sys.setenv(SPOTIFY_CLIENT_SECRET = '1bee2b2375da4f9a9332a114d31d0da7')
 access_token <- get_spotify_access_token()
 
-otra <- get_playlist_audio_features("", "6cZQQaJ0MsSHyUfGyuLgGK")
+otra1 <- get_playlist_audio_features("", "6cZQQaJ0MsSHyUfGyuLgGK")
 
-otra_j <- otra %>%
+otra_j <- otra1 %>%
   filter(added_by.id == '1153531084') %>%
   mutate(added_by.id = "Julia")
 
-otra_w <- otra %>%
+otra_w <- otra1 %>%
   filter(added_by.id == '1168120441') %>%
   mutate(added_by.id = "Willemijn")
 
-otra_r <- otra %>%
+otra_r <- otra1 %>%
   filter(added_by.id == 'roos.hutter') %>%
   mutate(added_by.id = "Roos")
 
-otra2 <- rbind(head(otra_r, 52), otra_w, head(otra_j, 52))
+otra <- rbind(head(otra_r, 52), otra_w, head(otra_j, 52))
 
-ggplot(otra2, aes(x = valence, y = energy)) + 
+ggplot(otra, aes(x = valence, y = energy)) + 
   geom_jitter() + 
   geom_smooth() +
   facet_wrap(~ added_by.id) +
   labs(x = "Valence", y = "Energy") +
   theme_light()
 
-otra_mean_median <- otra2 %>%
+otra_mean_median <- otra %>%
   group_by(added_by.id) %>%
   summarize(mean_energy = mean(energy),
             mean_valence = mean(valence),
@@ -43,12 +44,12 @@ otra_mean_median <- otra2 %>%
             median_loudness = median(loudness),
             median_tempo = median(tempo))
 
-ggplot(otra2, aes(x=added_by.id, y=acousticness)) +
+ggplot(otra, aes(x=added_by.id, y=acousticness)) +
   geom_boxplot() +
   labs(x = '', y = "Acousticness") +
   theme_light()
 
-otra2 |>                    # Start with awards.
+otra |>                    # Start with awards.
   mutate(
     mode = ifelse(mode == 0, "Minor", "Major")
   ) |>
@@ -88,4 +89,26 @@ otra2 |>                    # Start with awards.
     colour = "Mode",
     title = "On the road (again) analysis"
   )
+
+wood <-
+  get_tidy_audio_analysis("4XURqWF7mq3aW5iee3ATwD") |>
+  select(segments) |>
+  unnest(segments) |>
+  select(start, duration, pitches)
+
+wood |>
+  mutate(pitches = map(pitches, compmus_normalise, "euclidean")) |>
+  compmus_gather_chroma() |> 
+  ggplot(
+    aes(
+      x = start + duration / 2,
+      width = duration,
+      y = pitch_class,
+      fill = value
+    )
+  ) +
+  geom_tile() +
+  labs(x = "Time (s)", y = NULL, fill = "Magnitude") +
+  theme_minimal() +
+  scale_fill_viridis_c()
 
